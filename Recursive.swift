885c585765090847979157185813b857feca177c
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-11-29 21:35:59 +0000

    edited Recursive.swift

---
+++ Recursive.swift
@@ -0,0 +1,13 @@
+/*
+* The reverse program.
+*
+* @author Matthew Sanii
+* @version 1.0
+* @since 2021-29-11
+*
+*/
+
+print("Input what you want reversed: ")
+let input = readLine()
+let reverse = String(input!.reversed())
+print("reverse is ", reverse)
