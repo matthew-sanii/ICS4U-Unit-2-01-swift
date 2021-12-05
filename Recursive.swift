295ffb08c3d43ec8e6fd20b1ef49336b66bb05ad
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-12-05 00:48:35 +0000

    edited Recursive.swift

--- Recursive.swift
+++ Recursive.swift
@@ -8,9 +8,22 @@
 */
 
 print("Input what you want reversed: ")
-let usrinput = readLine()!
-var result = ""
-for i in usrinput {
-    result = "\(i)" + result
+var usrinput = readLine()!
+let task: ReverseString = ReverseString()
+usrinput = task.reverse(usrinput)
+print("inverted version of input is: ", usrinput)
+
+// this is the class that is used to reverse the string
+// (this took me way too long to figure out how to do)
+class ReverseString {
+    func reverseText(_ text: String, _ location: Int) -> String {
+        if (location >= 0) {
+            return String(Array(text)[location]) + 
+            self.reverseText(text, location - 1)
+        }
+        return ""
+    }
+    func reverse(_ text: String) -> String {
+        return self.reverseText(text, text.count - 1)
+    }
 }
-print("inverted version of input is: ", result)
